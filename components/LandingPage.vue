83d58f54b24b3015050034a01c11a39ff90a436e
Dimitar Pashovski <dipa@boozt.com>
2024-10-23 00:00:29 +0200

    make it goth!

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -2,9 +2,18 @@
   import './LandingPage.scss';
 </script>
 <template>
-  <div class="landing-page">
-    <div class="landing-page__image-wrap">
-      <img class="landing-page__image" src="/img/profile.jpg">
+  <main class="landing-page">
+    <div class="items-container">
+      <div class="item item--forest">
+        <div class="item__screen item__screen--full"></div>
+        <div class="item__screen item__screen--clip"></div>
+      </div>
+
+      <div class="item item--image">
+        <div class="item__screen item__screen--full"></div>
+        <div class="item__screen item__screen--clip"></div>
+      </div>
     </div>
-  </div>
+    <img class="landing-page__mirror" src="/img/mirror.png" alt="mirror"/>
+  </main>
 </template>
